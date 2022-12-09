661034f7d6b41e2ff9585c397cd839eb77280e91
Ирина <mi.irene97@gmail.com>
2022-12-09 12:34:26 +0300

    Initial commit

---
+++ src/main/java/course4/lesson1httprest/Lesson41HttpRestApplication.java
@@ -0,0 +1,13 @@
+package course4.lesson1httprest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lesson41HttpRestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lesson41HttpRestApplication.class, args);
+    }
+
+}
